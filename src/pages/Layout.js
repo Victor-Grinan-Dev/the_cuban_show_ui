8427c406a55a71acfce7dea8950bebad44e72d32
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-02-04 20:03:33 +0200

    create layout

---
+++ src/pages/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from '../components/Main';
+
+function Layout() {
+  return (
+    <div className='layout'>
+        <Main className='main'/>
+    </div>
+  )
+}
+
+export default Layout;

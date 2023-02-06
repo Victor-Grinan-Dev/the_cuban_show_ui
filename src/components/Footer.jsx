2244f0dac53dd85f9175854e4a6d4eaca21811df
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-02-06 21:12:43 +0200

    icons are working and are colorfull!!

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import style from './footerStyle.module.css';
 
 import f from '../assets/someIcons/f_logo_RGB-Blue_512.png';
+import ig from '../assets/someIcons/Instagram_Glyph_Gradient.png';
+import twitter from '../assets/someIcons/2021 Twitter logo - blue.png';
+import yt from '../assets/someIcons/yt_icon_rgb.png';
 
 const d = new Date();
 let year = d.getFullYear();
@@ -13,10 +16,23 @@ const Footer = () => {
           Follow us
         </div>
         <div className={style.some}>
-          <a href="https://www.facebook.com/TheCubanShow502/?show_switched_toast=0&show_invite_to_follow=0&show_switched_tooltip=0&show_podcast_settings=0&show_community_transition=0&show_community_review_changes=0&show_community_rollback=0&show_follower_visibility_disclosure=0" target="_blank" rel="noreferrer"><div className={style.f} style={{backgroundImage: `url(${f})`}}>f</div></a>
-          <a href="https://www.instagram.com/thecubanshow/" target="_blank" rel="noreferrer"><div className={style.ig}>Ig</div></a>
-          <a href="https://twitter.com/TheCubanShow502" target="_blank" rel="noreferrer"><div className={style.yt}>twitter</div></a>
-          <a href="https://www.youtube.com/channel/UCzjcr4N5avBbjEcywqVax_Q" target="_blank" rel="noreferrer"><div className={style.twitter}>You Tube</div></a>
+        
+          <a href="https://www.facebook.com/TheCubanShow502/?show_switched_toast=0&show_invite_to_follow=0&show_switched_tooltip=0&show_podcast_settings=0&show_community_transition=0&show_community_review_changes=0&show_community_rollback=0&show_follower_visibility_disclosure=0" target="_blank" rel="noreferrer">
+            <img className={style.f} src={f} alt="icon" />
+          </a>
+
+          <a href="https://www.instagram.com/thecubanshow/" target="_blank" rel="noreferrer">
+            <img className={style.ig} src={ig} alt="icon" />
+          </a>
+
+          <a href="https://twitter.com/TheCubanShow502" target="_blank" rel="noreferrer">
+            <img className={style.twitter} src={twitter} alt="icon" />
+          </a>
+
+          <a href="https://www.youtube.com/channel/UCzjcr4N5avBbjEcywqVax_Q" target="_blank" rel="noreferrer">
+            <img className={style.yt} src={yt} alt="icon" />
+          </a>
+        
         </div>
         <div className={style.copyrights}>
           <p>Copyrights© {year}</p>

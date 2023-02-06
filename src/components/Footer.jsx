38490dac0a173e4de49ac6a755fac41256265a7c
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-02-06 20:38:12 +0200

    rel=noreffer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,10 +13,10 @@ const Footer = () => {
           Follow us
         </div>
         <div className={style.some}>
-          <a href="https://www.facebook.com/TheCubanShow502/?show_switched_toast=0&show_invite_to_follow=0&show_switched_tooltip=0&show_podcast_settings=0&show_community_transition=0&show_community_review_changes=0&show_community_rollback=0&show_follower_visibility_disclosure=0" target="_blank"><div className={style.f} style={{backgroundImage: `url(${f})`}}>f</div></a>
-          <a href="https://www.instagram.com/thecubanshow/" target="_blank"><div className={style.ig}>Ig</div></a>
-          <a href="https://twitter.com/TheCubanShow502" target="_blank"><div className={style.yt}>twitter</div></a>
-          <a href="https://www.youtube.com/channel/UCzjcr4N5avBbjEcywqVax_Q" target="_blank"><div className={style.twitter}>You Tube</div></a>
+          <a href="https://www.facebook.com/TheCubanShow502/?show_switched_toast=0&show_invite_to_follow=0&show_switched_tooltip=0&show_podcast_settings=0&show_community_transition=0&show_community_review_changes=0&show_community_rollback=0&show_follower_visibility_disclosure=0" target="_blank" rel="noreferrer"><div className={style.f} style={{backgroundImage: `url(${f})`}}>f</div></a>
+          <a href="https://www.instagram.com/thecubanshow/" target="_blank" rel="noreferrer"><div className={style.ig}>Ig</div></a>
+          <a href="https://twitter.com/TheCubanShow502" target="_blank" rel="noreferrer"><div className={style.yt}>twitter</div></a>
+          <a href="https://www.youtube.com/channel/UCzjcr4N5avBbjEcywqVax_Q" target="_blank" rel="noreferrer"><div className={style.twitter}>You Tube</div></a>
         </div>
         <div className={style.copyrights}>
           <p>Copyrights© {year}</p>

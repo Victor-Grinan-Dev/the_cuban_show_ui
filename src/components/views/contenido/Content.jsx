57dc7db309bcd4b9e556def86dde9651f4c53b4d
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-02-13 22:29:13 +0200

    create addContent page

--- src/components/views/contenido/Content.jsx
+++ src/components/views/contenido/Content.jsx
@@ -5,9 +5,12 @@ import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
 
 import NewsCard from '../../UI/newsCard/NewsCard';
-import { contents } from '../../../fakeDB';
+//import { contents } from '../../../fakeDB';
 // import style from './styleContent.module.css';
 import genStyle from '../../../style/styleGeneral.module.css';
+import { useEffect, useState } from 'react';
+import { db } from '../../../firebase';
+import { collection, getDocs } from 'firebase/firestore';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -18,12 +21,24 @@ const Item = styled(Paper)(({ theme }) => ({
   }));
 
 const Content = () => {
+  const [content, setcontent] = useState([]);
+  const contentCollectionRef = collection(db, "content");
+  /* read the data */
+  useEffect(() => {
+    const getContents = async () => {
+      const data = await getDocs(contentCollectionRef);
+      setcontent(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+    }
+    getContents();
+  // eslint-disable-next-line
+  }, []);
+
   return (
     <div className={genStyle.view}>        
         <Box sx={{ width: '100%' }}>
             <Stack spacing={2}>
                 {
-                    contents && contents.map((c, i) => (
+                    content && content.map((c, i) => (
                         <Item key={i}><NewsCard props={c}/></Item>
                     ))
                 }

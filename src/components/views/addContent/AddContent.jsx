af7b325179f4a71bb515fd6be712a368bdf9b743
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-02-14 20:26:04 +0200

    update service file (del &  update)

--- src/components/views/addContent/AddContent.jsx
+++ src/components/views/addContent/AddContent.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import { createContent } from '../../../services/firebaseService';
+import genStyle from '../../../style/styleGeneral.module.css';
+import style from './addContent.module.css';
 
 const AddContent = () => {
     const [newTitle, setNewTitle] = useState('');
     const [newBody, setNewBody] = useState('');
 
   return (
-    <div style={{display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center'
-    }}>
-        <input type="text" placeholder='Title' onChange={(e)=>setNewTitle(e.target.value)}/>
-        <textarea name="body" id="body" cols="30" rows="10" placeholder='Body' onChange={(e)=>setNewBody(e.target.value)}></textarea>
+    <div className={genStyle.view}>
+        <input className={style.input} type="text" placeholder='Title' onChange={(e)=>setNewTitle(e.target.value)}/>
+        <input className={style.input} type="text" placeholder='Image Url' onChange={(e)=>setNewTitle(e.target.value)}/>
+        <input className={style.input} type="text" placeholder='Heading' onChange={(e)=>setNewTitle(e.target.value)}/>
+        <textarea className={style.textarea} name="body" id="body"  placeholder='Body' onChange={(e)=>setNewBody(e.target.value)}></textarea>
         <button onClick={() => createContent(newTitle, newBody) }>add</button>
     </div>
   )
--- src/services/firebaseService.js
+++ src/services/firebaseService.js
@@ -1,6 +1,6 @@
 
 import { db } from '../firebase';
-import { collection, getDocs, addDoc } from 'firebase/firestore';
+import { collection, getDocs, addDoc, updateDoc, doc, deleteDoc } from 'firebase/firestore';
 
 const contentCollectionRef = collection(db, "content");
 
@@ -13,3 +13,12 @@ export const getContents = async () => {
     return data.docs.map((doc) => ({...doc.data(), id: doc.id}));
   }
 
+export const updateContent = async (id, content) => {
+    const contentDoc = doc(db, "content", id);
+    await updateDoc(contentDoc, content);
+}
+
+export const deleteContent = async (id) => {
+    const contentDoc = doc(db, "content", id);
+    await deleteDoc(contentDoc);
+}

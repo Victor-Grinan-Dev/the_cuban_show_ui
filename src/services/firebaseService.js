a9ed1e36624329b21ee44b6a28a12f84131d7f88
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-02-14 19:59:44 +0200

    update service (add)

--- src/services/firebaseService.js
+++ src/services/firebaseService.js
@@ -1,10 +1,15 @@
 
 import { db } from '../firebase';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, addDoc } from 'firebase/firestore';
 
 const contentCollectionRef = collection(db, "content");
+
+export const createContent = async (newTitle, newBody) => {
+    await addDoc(contentCollectionRef, {title:newTitle, body: newBody});
+} 
 
 export const getContents = async () => {
     const data = await getDocs(contentCollectionRef);
     return data.docs.map((doc) => ({...doc.data(), id: doc.id}));
   }
+

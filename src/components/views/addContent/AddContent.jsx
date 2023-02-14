2a4895173c85733efdd9716f0eb6dad50b5fde3b
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-02-14 20:00:04 +0200

    update service (add)

--- src/components/views/addContent/AddContent.jsx
+++ src/components/views/addContent/AddContent.jsx
@@ -1,22 +1,16 @@
 import React, { useState } from 'react';
-import { collection, addDoc} from 'firebase/firestore';
-import { db }from '../../../firebase';
+import { createContent } from '../../../services/firebaseService';
 
 const AddContent = () => {
     const [newTitle, setNewTitle] = useState('');
     const [newBody, setNewBody] = useState('');
-    const userCollectionRef = collection(db, "content");
-
-    const createContent = async () => {
-        await addDoc(userCollectionRef, {title:newTitle, body: newBody});
-    } 
 
   return (
-    <div>
-        <p>AddContent</p>
+    <div style={{display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center'
+    }}>
         <input type="text" placeholder='Title' onChange={(e)=>setNewTitle(e.target.value)}/>
         <textarea name="body" id="body" cols="30" rows="10" placeholder='Body' onChange={(e)=>setNewBody(e.target.value)}></textarea>
-        <button onClick={createContent}>add</button>
+        <button onClick={() => createContent(newTitle, newBody) }>add</button>
     </div>
   )
 }

8daeeb80a0e1d922f14cce4a62ce3fd01130417b
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-02-25 16:41:37 +0200

    cleaned index

--- src/index.js
+++ src/index.js
@@ -10,20 +10,3 @@ root.render(
     <App />
   </Provider>
 );
-
-/*
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-
-import App from './App';
-import store from './app/store';
-;
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
-*/

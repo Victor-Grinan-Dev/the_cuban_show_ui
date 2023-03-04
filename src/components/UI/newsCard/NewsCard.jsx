1893a0d586926fa451e0dbb4f73be8a536e74da5
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-03-04 12:47:31 +0200

    created single card and link

--- src/components/UI/newsCard/NewsCard.jsx
+++ src/components/UI/newsCard/NewsCard.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import style from './newsCard.module.css';
 import testImg from '../../../assets/logo-black.jpg';
+import { Link } from 'react-router-dom';
 
 const NewsCard = ({props}) => { 
-  const {title} = props;
+  const {id, title, data} = props;
   //date?
   return (
-    <div className={style.newsCard}>
-        <img 
-        className={style.newsCardImg}
-        src={props?.image || testImg} 
-        alt="newsImage" 
-        />
-        <div className={style.contentContainer}>
-            <h3 className={style.newsCardTitle}>
-                {title || 'test title'}
-            </h3>
-            <hr className={style.line}/>
-            {/* <p>{body}</p> */}
-            <p className={style.date}>Published: {Date.now()}</p>
-        </div>
-    </div>
+    <Link
+      to={`articleId/${id}`} 
+      state={{data:data}}
+      >
+      <div className={style.newsCard}>
+          <img 
+          className={style.newsCardImg}
+          src={props?.image || testImg} 
+          alt="newsImage" 
+          />
+          <div className={style.contentContainer}>
+              <h3 className={style.newsCardTitle}>
+                  {title || 'test title'}
+              </h3>
+              <hr className={style.line}/>
+              {/* <p>{body}</p> */}
+              <p className={style.date}>Published: {Date.now()}</p>
+          </div>
+      </div>
+    </Link>
   )
 }
 
--- src/components/views/singlePage/SinglePage.jsx
+++ src/components/views/singlePage/SinglePage.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const SinglePage = () => {
+  const location = useLocation();
+  const data =  location.data;
+
+  useEffect(()=>{
+    console.log(data)
+  },[])
+
   return (
-    <div>SinglePage</div>
+    <div>
+      SinglePage
+    </div>
   )
 }
 

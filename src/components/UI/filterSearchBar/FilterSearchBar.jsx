ea355f2acae0392601d155ea9251cf6a30fc4d9f
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-03-08 22:01:36 +0200

    created tags add and del ui

--- src/components/UI/filterSearchBar/FilterSearchBar.jsx
+++ src/components/UI/filterSearchBar/FilterSearchBar.jsx
@@ -1,24 +1,71 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState } from 'react';
 import { allTags } from '../../../appConfig';
 import style from './filterSearchBar.module.css';
 
 const FilterSearchBar = () => {
   const [isShowSearchInput, setIsShowSearchInput] = useState(false);
+  const [tagsFilters, setTagsFilters] = useState([]);
+
+  useEffect(() => {
+    console.log(tagsFilters);
+  }, [tagsFilters]);
+
+  const addOrDelHandler = (tag) => {
+    isTagIncluded(tag) ? deleteTagFilterHandler(tag) : addTagsFiltershandler(tag)
+  }
+
+  const addTagsFiltershandler = (tag) => {
+      setTagsFilters([...tagsFilters, tag]);
+    };
+
+  const deleteTagFilterHandler = (tag) => {   
+    setTagsFilters(tagsFilters.filter(t => {
+        return t !== tag
+      }
+    ))
+  };
+
+  const isTagIncluded = (tag) => {
+    return tagsFilters.includes(tag)
+  }
+
   return (
     <div className={style.filterSearchBar}>
         <ul className={style.filterSearchList} >
             {!isShowSearchInput &&
               <>
-                <li className={style.filterSearchCriteria} >All</li>
-                <li className={style.filterSearchCriteria} >Cuba</li>
-                <li className={style.filterSearchCriteria} >Usa</li>
-                <li className={style.filterSearchCriteria} >World's</li>
-                <select className={style.filterSearchCriteria} 
+                <li className={style.filterSearchCriteria}
+                onClick={ ()=>setTagsFilters([]) } 
+                >All</li>
+
+                <li 
+                  id='cuba'
+                  className={style.filterSearchCriteria} 
+                  onClick={ (e) => addOrDelHandler(e.target.id)} 
+                >Cuba</li>
+                <li 
+                  id='usa'
+                  className={style.filterSearchCriteria} 
+                  onClick={ (e) => addOrDelHandler(e.target.id) }
+                >Usa </li>
+                <li 
+                id='world'
+                  className={style.filterSearchCriteria} 
+                  onClick={ (e) => addOrDelHandler(e.target.id) }
+                >World's</li>
+                <select  
                   style={{fontSize:"14px"}}
+                  onChange={(e) => addOrDelHandler(e.target.value)}
                 >
                   <option style={{fontSize:"10px"}} hidden>More tags</option>
                   {allTags && allTags.map((t, i) => (
-                    (t !== "usa" && t !=="cuba" && t !== "world") && <option key={i} style={{fontSize:"10px"}} value={t} >{t}</option>
+                    (t !== "usa" && t !=="cuba" && t !== "world") && <option key={i} 
+                    style={{fontSize:"10px"}} 
+                    value={t} 
+                    id={t}
+                    >
+                      {t}{isTagIncluded(t) && '✅'}
+                    </option>
                   ))}
                 </select>
               </>
@@ -33,7 +80,7 @@ const FilterSearchBar = () => {
                 </select>
               </li>
             }
-            <li className={style.filterSearchCriteria} on onClick={()=>setIsShowSearchInput(!isShowSearchInput)}>🔎</li>
+            <li className={style.filterSearchCriteria} onClick={()=>setIsShowSearchInput(!isShowSearchInput)}>🔎</li>
         </ul>
     </div>
   )

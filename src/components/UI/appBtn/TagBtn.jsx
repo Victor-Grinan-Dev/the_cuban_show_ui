f69d095f214913c591b2e1c4bfba6bd645b32c83
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-05-15 13:02:30 +0300

    fixed tags in addContent

--- src/components/UI/appBtn/TagBtn.jsx
+++ src/components/UI/appBtn/TagBtn.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 // import style from "./tagBtn.module.css";
 import AppBtn from "./AppBtn";
 import { selectedAppBtn } from "./standardStyle";
+
 const notSelected = {
   borderColor: "black",
   color: "black",
@@ -13,22 +14,16 @@ const selected = {
   margin: "0 5px",
 };
 
-const TagBtn = ({ fxPrimary, label }) => {
-  const [isSelected, setIsSelected] = useState(false);
-
-  const isSelectedHandle = () => {
-    setIsSelected(!isSelected);
-  };
+const TagBtn = ({ fxPrimary, label, style, isSelected }) => {
 
   return (
     <AppBtn
       type={"terceary"}
       fx={() => {
         fxPrimary();
-        isSelectedHandle();
       }}
       caption={label}
-      style={isSelected ? selected : notSelected}
+      style={style ? style : isSelected ? selected : notSelected}
     />
   );
 };
--- src/functions/tags.js
+++ src/functions/tags.js
@@ -1,4 +1,8 @@
    
    export const isTagIncluded = (tag, filterTags) => {
+    if(!filterTags){
+      return false;
+    }
+    console.log('does list includes ', tag, ' => ', filterTags.includes(tag));
     return filterTags.includes(tag);
   };

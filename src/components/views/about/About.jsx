e5c9b71f0cf1ed5479a6305c8b99b0c8bc759e37
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-05-28 18:19:56 +0300

    about installation

--- src/components/views/about/About.jsx
+++ src/components/views/about/About.jsx
@@ -6,6 +6,9 @@ import Banner from "../../UI/banner/Banner";
 import chromeTcs from "../../../assets/install-app/chorme_tcs_page.jpg";
 import zoom from "../../../assets/install-app/chrome_tcs_zoom.jpg";
 import settings from "../../../assets/install-app/chrome_tcs_settings.jpg";
+import modal from "../../../assets/install-app/chrome-confirm-modal.jpg"
+import toHomeSc from "../../../assets/install-app/phone-installed-app.jpg"
+import complete from "../../../assets/install-app/phone-added-to-home-app.jpg"
 
 export default function About() {
   return (
@@ -37,9 +40,9 @@ export default function About() {
       </p>
       <h4 className={style.installingTitle}>Installing the App in your mobile</h4>
 
-      <ol>
+      <ol className={style.list}>
         <li>
-          open the link of this app in your mobilephone with Google Chrome.
+          1. Open the link of this app in your mobilephone with Google Chrome.
         </li>
         <a href={chromeTcs} target="_blank" rel="noreferrer">
           <img
@@ -49,12 +52,13 @@ export default function About() {
           />
         </a>
         <li>
-          Click the settings in the right upper corner of the Chrome browser
+          2. Click the settings in the right upper corner of the Chrome browser
         </li>
         <a href={zoom} target="_blank" rel="noreferrer">
           <img src={zoom} alt="chrome settings" className={style.installImg} />
         </a>
-        <li>Select "Add to home screen" & accept the suggested name</li>
+        <li>
+          3. Select "install App". Notice that sometimes appears as "Add to home screen"</li>
         <a href={settings} target="_blank" rel="noreferrer">
           <img
             src={settings}
@@ -62,7 +66,30 @@ export default function About() {
             className={style.installImg}
           />
         </a>
+        <li>4. Install or accept the suggested name in the popup modal</li>
+        <a href={modal} target="_blank" rel="noreferrer">
+          <img
+            src={modal}
+            alt="chrome settings"
+            className={style.installImg}
+          />
+        </a>
+        <li>5. (Optional) Add the app to the mobile home screen </li>
+        <a href={toHomeSc} target="_blank" rel="noreferrer">
+          <img
+            src={toHomeSc}
+            alt="chrome settings"
+            className={style.installImg}
+          />
+        </a>
         <li>Enjoy the App from your mobile!</li>
+        <a href={complete} target="_blank" rel="noreferrer">
+          <img
+            src={complete}
+            alt="chrome settings"
+            className={style.installImg}
+          />
+        </a>
       </ol>
     </div>
   );

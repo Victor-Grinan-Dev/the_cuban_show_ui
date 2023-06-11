4cb1cb32770479a7b1d07c2550466a68aae7574c
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-06-11 22:29:22 +0300

    advCard update

--- src/components/UI/advCard/AdvCard.jsx
+++ src/components/UI/advCard/AdvCard.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import style from "../newsCard/newsCard.module.css";
 const AdvCard = (props) => {
-  const { text } = props;
+  const { text, dataAdSlot } = props; 
   return (
     <div className={style.advCard}>
       <div className={style.advContent}>{text}</div>
+      <>
+            <ins className="adsbygoogle"
+                style={{ display: "block" }}
+                data-ad-client="ca-pub-XXXXXXXXXXXXXXX"
+                data-ad-slot={dataAdSlot}
+                data-ad-format="auto"
+                data-full-width-responsive="true">
+            </ins>
+        </>
     </div>
   );
 };

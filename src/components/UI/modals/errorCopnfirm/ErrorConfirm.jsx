fb4e3bd25a68b4e1d4f88a224248dcb1dbfed771
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-07-03 14:12:15 +0300

    modal update css

--- src/components/UI/modals/errorCopnfirm/ErrorConfirm.jsx
+++ src/components/UI/modals/errorCopnfirm/ErrorConfirm.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-
+import style from '../modals.module.css';
 const ErrorConfirm = () => {
 
     const error = useSelector(state => state.app.error);
 
   return (
-    <div>
+    <div className={style.modalContentSize}>
         <p style={{color:"white", fontSize: "25px"}}>{error}</p>
     </div>
   )

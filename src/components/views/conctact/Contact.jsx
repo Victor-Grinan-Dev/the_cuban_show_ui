09cbe6e4d7ed13f071693a76ebcb7a38f17a5f89
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-08-17 10:51:50 +0300

    contact

--- src/components/views/conctact/Contact.jsx
+++ src/components/views/conctact/Contact.jsx
@@ -1,27 +1,55 @@
 import * as React from "react";
 import genStyle from "../../../style/styleGeneral.module.css";
 import Banner from "../../UI/banner/Banner";
+import { useSelector } from "react-redux";
+import { translate } from "../../../translation/translation";
+
+// const page = {
+//   backgroundColor: "white",
+//   color: "#252525",
+//   padding: "0 10px",
+//   // backgroundImage: "none",
+// };
+
+// const pageDark = {
+//   backgroundColor: "#252525",
+//   color: "white",
+//   padding: "0 5px",
+//   // backgroundImage: "none",
+// };
 
 export default function Contact() {
+  // const darkMode = useSelector((state) => state.app.darkMode);
+  const currentLang = useSelector((state) => state.app.currentLang);
+  const link = { color: "blue" };
   return (
     <div className={genStyle.view}>
       <Banner />
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore numquam
-        sequi dolor atque aspernatur est perspiciatis consequuntur dolores
-        laborum asperiores maxime ex corporis, velit animi iusto eaque quae
-        quaerat? Temporibus aperiam perferendis reiciendis, aut cumque doloribus
-        accusantium error consequatur illum, exercitationem libero repellat. Nam
-        architecto laudantium eveniet quos, earum soluta doloremque perferendis,
-        ratione aliquam, quia ducimus id! Eveniet pariatur, tenetur voluptatum
-        quos reiciendis earum consequatur nisi odio recusandae consequuntur ad
-        porro eligendi libero excepturi ullam laborum neque? Nemo libero modi,
-        perferendis nam impedit vel illum reiciendis enim maiores odio magni
-        voluptatibus laudantium aperiam, quae debitis commodi eveniet numquam
-        dolorem facilis?
-      </p>
-      <br />
-      <p style={{color:"blue"}}>thecubanshow502@gmail.com</p>
+      <div
+      // style={darkMode ? pageDark : page}
+      >
+        <h2 style={{ textTransform: "uppercase" }}>
+          {translate("contact us", currentLang)}:
+        </h2>
+        <br />
+
+        <p>
+          {translate("Production and direction", currentLang)}:{" "}
+          <span style={link}>Vladimir Aguilar</span>
+        </p>
+        <br />
+        <p>
+          {translate("Contents", currentLang)}:<span style={link}> ... </span>
+        </p>
+        <br />
+        <p>
+          {translate("Web app creation and maintenance", currentLang)}:{" "}
+          <span style={link}>Victor Griñán</span>{" "}
+        </p>
+
+        <br />
+        <p style={link}>thecubanshow502@gmail.com</p>
+      </div>
     </div>
   );
 }

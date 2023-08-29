74631160c5a4a91eb9dcc4e461669c3b779ae8a2
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-08-29 11:38:13 +0300

    adsense attempt 2

--- src/components/UI/advCard/AdvCard.jsx
+++ src/components/UI/advCard/AdvCard.jsx
@@ -1,20 +1,10 @@
 import React from "react";
 import style from "../newsCard/newsCard.module.css";
 const AdvCard = (props) => {
-  //const { text, dataAdSlot } = props;
+  const { text } = props;
   return (
     <div className={style.advCard}>
-      {/* <div className={style.advContent}>{text}</div> */}
-      {/* <>
-        <ins
-          className="adsbygoogle"
-          style={{ display: "block" }}
-          data-ad-client="ca-pub-5610388120909625"
-          data-ad-slot={dataAdSlot}
-          data-ad-format="auto"
-          data-full-width-responsive="true"
-        ></ins>
-      </> */}
+      <div className={style.advContent}>{text}</div>
       <script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6878852947463746"
